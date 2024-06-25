3cdda86b07cebd72f14686f4588b1595a66d50c9
Mark <mark.njau@student.moringaschool.com>
2024-06-26 01:07:30 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+
+// saturdayFun function declaration
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// mondayWork function expression
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+// wrapedAdjective function
+function wrapAdjective(wrapper ="*") {
+    return function(adjective = "special") {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    }
+}
+
+// Testing the functions
+console.log(saturdayFun()); // This saturday, I want to roller-skate!
+console.log(saturdayFun("bathe my dog")); // This Saturday, I want to bathe my dog!
+
+console.log(mondayWork()); // This Monday, I will go to the office.
+console.log(mondayWork("work from home")); // This Monday, I will work from home.
+
+const wrapInStars = wrapAdjectives('*')
+console.log(wrapInsTARS("a hard worker")) // You are *a hard worker*!
+
+const wrapInPipes = wrapAdjectives('||');
+console.log(wrapInPipes("a dedicated programmer")) // You are ||a dedicated programmer||!
+
